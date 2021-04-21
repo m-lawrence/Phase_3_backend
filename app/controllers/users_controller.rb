106a3d3a04a10827e59cc7862da5da56b68f6dae
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-21 15:00:44 -0400

    small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user, include: [:name, :age, :location]
     end
+
+    def new 
+        user = User.new
+        render json: user
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :age, :location)
+    end
+
 end
