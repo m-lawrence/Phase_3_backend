c7050ff70c99a9de32ad98323c8cb5f002aec716
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-21 17:42:38 -0400

    small changes

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :username, :description, :rating, :date
+  attributes :id, :username, :description, :rating, :date, :user_id, :hike_id
+  
 end
