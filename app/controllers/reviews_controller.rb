d2c222e1473f332c5ce2113fb1f3ec691179cf48
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-22 02:12:53 -0400

    whats going on

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
     before_action :find_review, only: [:show, :edit, :update, :destroy]
+    
     def index
         reviews = Review.all
         render json: reviews
@@ -8,14 +9,7 @@ class ReviewsController < ApplicationController
     def show
         review = Review.find(params[:id])
         render json: review
-        # if review
-        #     render json: review.to_json(:include =>{
-        #         :user => {:only => [:name, :age, :location, :myhikes, :id]},
-        #         :hike => {:only => [:averagerating, :name, :location, :image, :difficulty, :distance, :id]}
-        #         },
-        #         :except =>[:updated_at] 
-        #     )
-        # end
+       
     end
 
     def create
@@ -27,11 +21,14 @@ class ReviewsController < ApplicationController
     end
 
     def update
-        review.update(description: params[:description], rating: params[:rating])
+        # byebug
+        review.update(review_params)
+        
+        # review.update(description: params[:description], rating: params[:rating])
     end
 
     def destroy
-        review.destroy!
+        review.destroy
     end
 
     private
